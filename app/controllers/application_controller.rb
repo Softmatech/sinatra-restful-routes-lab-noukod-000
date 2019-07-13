877e1c0c06197fc7e122ce347075a9a3c2841cae
Andy Feidje Joseph <ingandy75@gmail.com>
2019-07-13 06:10:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,12 @@ get '/recipes/:id/edit' do
   erb :edit
 end
 
+patch '/recipes/:id' do
+  @recipe = Recipe.find(params[:id])
+  @recipe.name == params[:name]
+  @recipe.ingredients = params[:ingredients]
+  @recipe.cook_time = params[:cook_time]
+  @recipe.save
+end
+
 end
